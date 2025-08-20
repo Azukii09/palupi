5560a9994d1b7deb3a129ded75397ad89802372b
Azukii09 <azuknisianta@gmail.com>
2025-08-20 17:47:17 +0700

    #1 - **feat(frontend): add reusable BasicCard component and integrate into DashboardPage**
    - Create `BasicCard` component with `title`, `content`, and `footer` subcomponents.
    - Replace existing layout in `(admin)/dashboard/page.tsx` with `BasicCard` for improved structure and reuse.

--- frontend/app/[locale]/(admin)/dashboard/page.tsx
+++ frontend/app/[locale]/(admin)/dashboard/page.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import Button from "@/component/util/base/Button";
+import BasicCard from "@/component/util/base/BasicCard";
 
 export default function DashboardPage() {
   return (
-    <div className={"flex flex-wrap gap-4"}>
-      <Button buttonName={"default"} buttonType={"button"} variant={"default"} buttonText={"Default"} size={"md"}/>
-      <Button buttonName={"white"} buttonType={"button"} variant={"white"} buttonText={"White"} size={"md"}/>
-      <Button buttonName={"primary"} buttonType={"button"} variant={"primary"} buttonText={"Primary"} size={"md"}/>
-      <Button buttonName={"secondary"} buttonType={"button"} variant={"secondary"} buttonText={"Secondary"} size={"md"}/>
-      <Button buttonName={"success"} buttonType={"button"} variant={"success"} buttonText={"Success"} size={"md"}/>
-      <Button buttonName={"waring"} buttonType={"button"} variant={"warning"} buttonText={"Warning"} size={"md"}/>
-      <Button buttonName={"danger"} buttonType={"button"} variant={"danger"} buttonText={"Danger"} size={"md"}/>
-      <Button buttonName={"info"} buttonType={"button"} variant={"info"} buttonText={"Info"} size={"md"}/>
-      <Button buttonName={"link"} buttonType={"button"} variant={"link"} buttonText={"Link"} size={"md"}/>
-    </div>
+    <BasicCard>
+      {/*Title*/}
+      <BasicCard.title>
+        <h1 className="text-xl font-bold text-primary capitalize">
+          tes
+        </h1>
+      </BasicCard.title>
+
+      {/*content*/}
+      <BasicCard.content>
+        <div className={"flex gap-4"}>
+          <Button buttonName={"default"} buttonType={"button"} variant={"default"} buttonText={"Default"} size={"md"}/>
+          <Button buttonName={"white"} buttonType={"button"} variant={"white"} buttonText={"White"} size={"md"}/>
+          <Button buttonName={"primary"} buttonType={"button"} variant={"primary"} buttonText={"Primary"} size={"md"}/>
+          <Button buttonName={"secondary"} buttonType={"button"} variant={"secondary"} buttonText={"Secondary"} size={"md"}/>
+          <Button buttonName={"success"} buttonType={"button"} variant={"success"} buttonText={"Success"} size={"md"}/>
+          <Button buttonName={"waring"} buttonType={"button"} variant={"warning"} buttonText={"Warning"} size={"md"}/>
+          <Button buttonName={"danger"} buttonType={"button"} variant={"danger"} buttonText={"Danger"} size={"md"}/>
+          <Button buttonName={"info"} buttonType={"button"} variant={"info"} buttonText={"Info"} size={"md"}/>
+          <Button buttonName={"link"} buttonType={"button"} variant={"link"} buttonText={"Link"} size={"md"}/>
+        </div>
+      </BasicCard.content>
+
+      {/*Footer*/}
+      <BasicCard.footer>
+        <div className="text-md font-semibold text-admin-title capitalize">Footer</div>
+      </BasicCard.footer>
+    </BasicCard>
+
   );
 }
---
+++ frontend/component/util/base/BasicCard.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const BasicCardTitle = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="bg-white px-6 py-4 rounded-t-md text-primary w-full border-b-1 border-slate-200">
+            {children}
+        </div>
+    );
+};
+
+const BasicCardContent = ({ children, haveFooter = true }: { children: React.ReactNode, haveFooter?: boolean }) => {
+    return (
+        <div className={`${!haveFooter && "rounded-b-md"} bg-white p-6 text-primary w-full overflow-auto`}>{children}</div>
+    );
+};
+
+const BasicCardFooter = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="bg-white px-6 py-4 rounded-b-md text-primary w-full border-t-1 border-slate-200">
+            {children}
+        </div>
+    );
+};
+
+// Komponen utama Card1
+const BasicCard = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="rounded-md text-primary w-full shadow-md shadow-primary/40">
+            {children}
+        </div>
+    );
+};
+
+// Menambahkan properti statis
+BasicCard.title = BasicCardTitle;
+BasicCard.content = BasicCardContent;
+BasicCard.footer = BasicCardFooter;
+
+export default BasicCard;
