e669f91fa3f606ec0c4081a357ebe46dfa2da72c
Azukii09 <azuknisianta@gmail.com>
2025-08-20 17:39:19 +0700

    #1 - **feat(frontend): add reusable Button component and integrate into DashboardPage**
    - Create a `Button` component with support for multiple variants, sizes, and states.
    - Integrate the `Button` component into `(admin)/dashboard/page.tsx` for demonstration.
    - Update secondary color in `globals.css` to enhance design consistency.

--- frontend/app/[locale]/(admin)/dashboard/page.tsx
+++ frontend/app/[locale]/(admin)/dashboard/page.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Button from "@/component/util/base/Button";
 
 export default function DashboardPage() {
   return (
-    <div>
-      ini halaman dashboard
+    <div className={"flex flex-wrap gap-4"}>
+      <Button buttonName={"default"} buttonType={"button"} variant={"default"} buttonText={"Default"} size={"md"}/>
+      <Button buttonName={"white"} buttonType={"button"} variant={"white"} buttonText={"White"} size={"md"}/>
+      <Button buttonName={"primary"} buttonType={"button"} variant={"primary"} buttonText={"Primary"} size={"md"}/>
+      <Button buttonName={"secondary"} buttonType={"button"} variant={"secondary"} buttonText={"Secondary"} size={"md"}/>
+      <Button buttonName={"success"} buttonType={"button"} variant={"success"} buttonText={"Success"} size={"md"}/>
+      <Button buttonName={"waring"} buttonType={"button"} variant={"warning"} buttonText={"Warning"} size={"md"}/>
+      <Button buttonName={"danger"} buttonType={"button"} variant={"danger"} buttonText={"Danger"} size={"md"}/>
+      <Button buttonName={"info"} buttonType={"button"} variant={"info"} buttonText={"Info"} size={"md"}/>
+      <Button buttonName={"link"} buttonType={"button"} variant={"link"} buttonText={"Link"} size={"md"}/>
     </div>
   );
 }
