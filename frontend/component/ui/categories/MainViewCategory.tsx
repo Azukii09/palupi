5837b27745bae37bd18d97f75c7a10e432e983d3
Azukii09 <azuknisianta@gmail.com>
2025-08-22 15:18:23 +0700

    #2, #8 - **feat(frontend): update Table hover and strip colors, add action buttons to MainViewCategory**
    - Adjusted `Table` hover and strip colors for enhanced visual consistency.
    - Added action buttons (`Detail`, `Edit`, `Delete`) with icons in `MainViewCategory` table for row-specific interactions.

--- frontend/component/ui/categories/MainViewCategory.tsx
+++ frontend/component/ui/categories/MainViewCategory.tsx
@@ -4,6 +4,9 @@ import BasicCard from "@/component/util/base/BasicCard";
 import {useParams} from "next/navigation";
 import {BasicTableData} from "@/lib/data/dummy/BasicTable";
 import Table from "@/component/util/base/Table";
+import Button from "@/component/util/base/Button";
+import {FaEye, FaTrash} from "react-icons/fa";
+import {HiOutlinePencilAlt} from "react-icons/hi";
 
 export default function MainViewCategory() {
   const params = useParams()
@@ -38,6 +41,45 @@ export default function MainViewCategory() {
           }
           rowHover
           pagination={{paginated: true,}}
+          withActions={
+            (row:number|unknown)=>(
+              <td className="text-center flex items-center justify-center gap-2 py-2">
+                <Button
+                  buttonName={"detail"}
+                  buttonType={"button"}
+                  variant={"primary"}
+                  buttonText={"Detail"}
+                  size={"sm"}
+                  isBadge
+                  onlyIcon
+                  icon={<FaEye className={"size-4"}/>}
+                  handler={()=>console.log(row)}
+                />
+                <Button
+                  buttonName={"edit"}
+                  buttonType={"button"}
+                  variant={"warning"}
+                  buttonText={"Edit"}
+                  size={"sm"}
+                  isBadge
+                  onlyIcon
+                  icon={<HiOutlinePencilAlt className={"size-4"}/>}
+                  handler={()=>console.log(row)}
+                />
+                <Button
+                  buttonName={"delete"}
+                  buttonType={"button"}
+                  variant={"danger"}
+                  buttonText={"Delete"}
+                  size={"sm"}
+                  isBadge
+                  onlyIcon
+                  icon={<FaTrash className={"size-4"}/>}
+                  handler={()=>console.log(row)}
+                />
+              </td>
+            )
+          }
         />
       </BasicCard.content>
     </BasicCard>
