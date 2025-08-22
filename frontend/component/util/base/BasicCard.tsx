22fd659254937fd02661c84ac6570b2974f73e45
Azukii09 <azuknisianta@gmail.com>
2025-08-22 13:58:59 +0700

    ## - **refactor(frontend): adjust BasicCard content padding and structure**
    - Updated padding styles in `BasicCardContent` for better consistency.
    - Wrapped `children` with a new container to refine layout handling.

--- frontend/component/util/base/BasicCard.tsx
+++ frontend/component/util/base/BasicCard.tsx
@@ -10,7 +10,11 @@ const BasicCardTitle = ({ children }: { children: React.ReactNode }) => {
 
 const BasicCardContent = ({ children, haveFooter = true }: { children: React.ReactNode, haveFooter?: boolean }) => {
     return (
-        <div className={`${!haveFooter && "rounded-b-md"} bg-white p-6 text-primary w-full overflow-auto`}>{children}</div>
+        <div className={`${!haveFooter && "rounded-b-md"} bg-white py-5 px-3 text-primary w-full`}>
+          <div className={"py-2 px-1 overflow-auto"}>
+            {children}
+          </div>
+        </div>
     );
 };
 
