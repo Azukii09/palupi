6f682c81253ea648dd9aaba399acddd9250cb9c3
Azukii09 <azuknisianta@gmail.com>
2025-08-23 18:07:51 +0700

    ## - **feat(frontend): integrate Modal component into MainViewCategory action buttons**
    - Added `Modal` component to `MainViewCategory` for displaying category details.
    - Replaced standalone action buttons with `Modal` for improved interaction and context management.

--- frontend/component/ui/categories/MainViewCategory.tsx
+++ frontend/component/ui/categories/MainViewCategory.tsx
@@ -8,6 +8,7 @@ import {FaEye, FaTrash} from "react-icons/fa";
 import {HiOutlinePencilAlt} from "react-icons/hi";
 import {CategoryActiveSwitch} from "@/component/ui/categories/CategoryActiveSwitch";
 import {DummyCategory} from "@/lib/data/dummy/Category";
+import Modal from "@/component/util/base/Modal";
 
 export default function MainViewCategory() {
   const params = useParams()
@@ -47,7 +48,33 @@ export default function MainViewCategory() {
         withActions={
           (row: number | unknown) => (
             <td className="text-center flex items-center justify-center gap-2 py-2">
-            <Button
+                <Modal
+                  id={`detail-${row}`}
+                  btnVariant={"primary"}
+                  btnName={`detail-${row}`}
+                  btnText={`View`}
+                  btnSize={"xs"}
+                  btnBadge
+                  btnOnlyIcon
+                  btnIcon={<FaEye className={"size-4"}/>}
+                >
+                  <Modal.Header>
+                    Category Detail
+                  </Modal.Header>
+                  <Modal.Body>
+                    <div className={"flex flex-col gap-2"}></div>
+                  </Modal.Body>
+                  <Modal.Footer
+                    btnVariant={"success"}
+                    btnText={"Submit"}
+                    btnName={"Submit"}
+                    btnVariantType={"solid"}
+                    btnSize={"sm"}
+                    >
+                    Footer
+                  </Modal.Footer>
+                </Modal>
+                <Button
                   buttonName={"detail"}
                   buttonType={"button"}
                   variant={"primary"}
