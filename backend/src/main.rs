f8dc32068d68be0fa783fec4494d77185553c40b
Azukii09 <azuknisianta@gmail.com>
2025-08-24 10:17:30 +0700

    ## - **refactor(backend): simplify `main.rs` and remove unused dependencies**
    - Replaced Axum-based HTTP server setup in `main.rs` with a simple "Hello, world!" example.
    - Removed unused dependencies (`axum`, `serde`, `tokio`, `uuid`) from `Cargo.toml` and `Cargo.lock` for cleanup.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,27 +1,3 @@
-use axum::Router;
-use axum::routing::{delete, get, post, put};
-
-#[tokio::main]
-async fn main() {
-    let router01 = Router::new()
-        .route("/api/category", get(get_category))
-        .route("/api/category", put(put_category))
-        .route("/api/category", delete(delete_category))
-        .route("/api/category", post(post_category));
-
-    //IP and port
-    let address = "0.0.0.0:8080";
-    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
-
-    axum::serve(listener,router01).await.unwrap();
+fn main() {
+    println!("Hello, world!");
 }
-
-async fn get_category() {
-    println!("Hello, from category!");
-}
-
-async fn post_category() {}
-
-async fn put_category() {}
-
-async fn delete_category() {}
