42f99893c102bd0eb7ad0bacfa3a8a3ff69fd9b5
Azukii09 <azuknisianta@gmail.com>
2025-08-25 16:55:20 +0700

    #2, #9 - **feat(frontend): enhance CategoryDetail with dynamic data and table structure**
    - Integrated `useParams` to fetch locale-specific category data.
    - Added table structure for a more organized detail display.
    - Updated `CategoryDetail` to dynamically show category name based on provided ID.

--- frontend/component/ui/categories/detail/CategoryDetail.tsx
+++ frontend/component/ui/categories/detail/CategoryDetail.tsx
@@ -2,12 +2,18 @@
 import React from 'react';
 import {FaEye} from "react-icons/fa";
 import Modal from "@/component/util/base/Modal";
+import {DummyCategory} from "@/lib/data/dummy/Category";
+import {useParams} from "next/navigation";
+import {delaGothic, poppins} from "@/lib/font/font";
 
 export default function CategoryDetail({
   id
 }:{
   id:number;
 }) {
+  const params = useParams()
+
+  const data = DummyCategory.find(item => item.locale === params.locale)?.data.find(it => it.id === id);
   return (
     <Modal
       id={`detail-${id}`}
@@ -24,18 +30,23 @@ export default function CategoryDetail({
       </Modal.Header>
       <Modal.Body>
         <div className={"flex flex-col gap-2"}>
-          tes {id}
+          <table className={` w-full rounded-xl p-6 text-sm`}>
+            <tbody className={`${poppins.className} rounded-xl`}>
+              <tr
+                className={` border-y rounded-xl`}
+              >
+                <td className={`border-primary px-4 py-2 border-x rounded-xl`}>
+                  tes
+                </td>
+                <td className={`border-primary px-4 py-2 border-x `}>
+                  tes 2
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          {data?.name}
         </div>
       </Modal.Body>
-      <Modal.Footer
-        btnVariant={"success"}
-        btnText={"Submit"}
-        btnName={"Submit"}
-        btnVariantType={"solid"}
-        btnSize={"sm"}
-      >
-        Footer
-      </Modal.Footer>
     </Modal>
   );
 }
