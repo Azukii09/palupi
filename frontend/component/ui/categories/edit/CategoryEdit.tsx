d538765c17e510a0997fea898b611818434e636c
Azukii09 <azuknisianta@gmail.com>
2025-08-26 12:37:48 +0700

    #2, #10 - **feat(frontend): implement Modal with edit functionality in CategoryEdit**
    - Added `Modal` component with UI structure including header, body, and footer.
    - Integrated `HiOutlinePencilAlt` icon for the edit button.
    - Enabled dynamic `id` prop to tailor Modal content for specific categories.

--- frontend/component/ui/categories/edit/CategoryEdit.tsx
+++ frontend/component/ui/categories/edit/CategoryEdit.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import {HiOutlinePencilAlt} from "react-icons/hi";
+import Modal from "@/component/util/base/Modal";
 
-export default function CategoryEdit() {
+export default function CategoryEdit({
+  id,
+}:{
+  id:number;
+}) {
   return (
-    <div></div>
+    <Modal
+      id={`edit-${id}`}
+      btnVariant={"warning"}
+      btnName={`edit-${id}`}
+      btnText={`Edit`}
+      btnSize={"xs"}
+      btnBadge
+      btnOnlyIcon
+      btnIcon={<HiOutlinePencilAlt className={"size-4"}/>}
+    >
+      <Modal.Header>
+        Edit Category
+      </Modal.Header>
+      <Modal.Body>
+        <div className={"flex flex-col gap-2"}>
+          tes {id as number}
+        </div>
+      </Modal.Body>
+      <Modal.Footer
+        btnVariant={"success"}
+        btnText={"Submit"}
+        btnName={"Submit"}
+        btnVariantType={"solid"}
+        btnSize={"sm"}
+      >
+        Footer
+      </Modal.Footer>
+    </Modal>
   );
 }
