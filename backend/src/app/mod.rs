c5d35f1fd7530201c7d67fd97c952d58a31f7d30
Azukii09 <azuknisianta@gmail.com>
2025-08-27 11:29:15 +0700

    ## - **feat(backend): add `category_router` for modular category route handling**
    - Created `category_router` function in `route.rs` for category-specific routing logic.
    - Updated `build_router` method to integrate the new `category_router`.

--- backend/src/app/mod.rs
+++ backend/src/app/mod.rs
@@ -3,9 +3,8 @@ use axum::Router;
 pub mod route;
 
 pub fn build_router() -> Router {
-    // /api/v1/* diisi oleh sub-router
     let api_v1 = Router::new()
-        .nest("/category", route::router());
+        .nest("/category", route::category_router());
 
     Router::new().nest("/api/v1", api_v1)
 }
---
+++ backend/src/app/route.rs
@@ -0,0 +1,8 @@
+use axum::{
+    routing::{ get},
+     Router,
+};
+
+pub fn category_router() -> Router {
+    Router::new().route("/", get(|| async { "Hello category pisah!" }))
+}
