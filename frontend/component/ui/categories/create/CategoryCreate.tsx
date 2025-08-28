2cfd6a70f5ff7ac4a46ff42649cd2e51fcd90df9
Azukii09 <azuknisianta@gmail.com>
2025-08-28 13:11:33 +0700

    #2, #7 - **feat(frontend): add "New Category" modal to CategoryCreate component**
    - Integrated reusable `Modal` component into `CategoryCreate` for adding new categories.
    - Added modal header, body, and footer to improve structure and usability.

--- frontend/component/ui/categories/create/CategoryCreate.tsx
+++ frontend/component/ui/categories/create/CategoryCreate.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Modal from "@/component/util/base/Modal";
 
 export default function CategoryCreate() {
   return (
-    <div></div>
+    <Modal id={"mdl-add"} btnVariant={"primary"} btnName={"add-category"} btnText={"New Category"} btnSize={"sm"}>
+      <Modal.Header>
+        Add New Category
+      </Modal.Header>
+      <Modal.Body>body</Modal.Body>
+      <Modal.Footer
+        btnVariant={"success"}
+        btnText={"Submit"}
+        btnName={"Submit"}
+        btnVariantType={"solid"}
+        btnSize={"sm"}
+      >
+      </Modal.Footer>
+    </Modal>
   );
 }
