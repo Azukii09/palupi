892d251ec37ed073ee95adf125f7a50638742c86
Azukii09 <azuknisianta@gmail.com>
2025-08-28 13:08:00 +0700

    #2, #7 - **feat(frontend): add "Add New Category" button to MainViewCategory**
    - Introduced a primary button for adding new categories in `MainViewCategory`.
    - Adjusted table layout to accommodate the new button for improved usability.

--- frontend/component/ui/categories/MainViewCategory.tsx
+++ frontend/component/ui/categories/MainViewCategory.tsx
@@ -9,6 +9,7 @@ import {DummyCategory} from "@/lib/data/dummy/Category";
 import Modal from "@/component/util/base/Modal";
 import CategoryDetail from "@/component/ui/categories/detail/CategoryDetail";
 import CategoryEdit from "@/component/ui/categories/edit/CategoryEdit";
+import Button from "@/component/util/base/Button";
 
 export default function MainViewCategory() {
   const params = useParams()
@@ -27,73 +28,76 @@ export default function MainViewCategory() {
       <BasicCard.content
         haveFooter={false}
       >
-      <Table
-        data={dataInLocal}
-        excludesColumnsData={[ "id"]}
-        excludesColumnsName={[ "id"]}
-        variants={"strip"}
-        withNumbering
-        withSearch={
-          {
-            searchStatus: true,
-            textColor: "text-primary",
-            borders: "border-primary",
-          }
-        }
-        rowHover
-        pagination={{
-          paginated: true,
-          paginatedPageCount:[5,10,15],
-        }}
-        withActions={
-          (row: number | unknown) => (
-            <td className="text-center flex items-center justify-center gap-2 py-2">
-              <CategoryDetail id={row as number}/>
+        <div>
+          <Button buttonType={"button"} variant={"primary"} buttonName={"new-category"} buttonText={"Add New Category"} size={"sm"}/>
+          <Table
+            data={dataInLocal}
+            excludesColumnsData={[ "id"]}
+            excludesColumnsName={[ "id"]}
+            variants={"strip"}
+            withNumbering
+            withSearch={
+              {
+                searchStatus: true,
+                textColor: "text-primary",
+                borders: "border-primary",
+              }
+            }
+            rowHover
+            pagination={{
+              paginated: true,
+              paginatedPageCount:[5,10,15],
+            }}
+            withActions={
+              (row: number | unknown) => (
+                <td className="text-center flex items-center justify-center gap-2 py-2">
+                  <CategoryDetail id={row as number}/>
 
-              <CategoryEdit id={row as number}/>
+                  <CategoryEdit id={row as number}/>
 
-              <Modal
-                id={`delete-${row}`}
-                btnVariant={"danger"}
-                btnName={`delete-${row}`}
-                btnText={`Delete`}
-                btnSize={"xs"}
-                btnBadge
-                btnOnlyIcon
-                btnIcon={<FaTrash className={"size-4"}/>}
-              >
-                <Modal.Header>
-                  Delete Category
-                </Modal.Header>
-                <Modal.Body>
-                  <div className={"flex flex-col gap-2"}>
-                    tes {row as number}
-                  </div>
-                </Modal.Body>
-                <Modal.Footer
-                  btnVariant={"success"}
-                  btnText={"Submit"}
-                  btnName={"Submit"}
-                  btnVariantType={"solid"}
-                  btnSize={"sm"}
-                >
-                  Footer
-                </Modal.Footer>
-              </Modal>
-              </td>
-            )
-          }
-        customColumnRenderer={{
-          status:(data)=>(
-            <div className={"flex items-center justify-center"}>
-              <CategoryActiveSwitch
-                categoryId={data.id as string}
-                initialActive={data.status as boolean}
-              />
-            </div>
-          )
-        }}
-        />
+                  <Modal
+                    id={`delete-${row}`}
+                    btnVariant={"danger"}
+                    btnName={`delete-${row}`}
+                    btnText={`Delete`}
+                    btnSize={"xs"}
+                    btnBadge
+                    btnOnlyIcon
+                    btnIcon={<FaTrash className={"size-4"}/>}
+                  >
+                    <Modal.Header>
+                      Delete Category
+                    </Modal.Header>
+                    <Modal.Body>
+                      <div className={"flex flex-col gap-2"}>
+                        tes {row as number}
+                      </div>
+                    </Modal.Body>
+                    <Modal.Footer
+                      btnVariant={"success"}
+                      btnText={"Submit"}
+                      btnName={"Submit"}
+                      btnVariantType={"solid"}
+                      btnSize={"sm"}
+                    >
+                      Footer
+                    </Modal.Footer>
+                  </Modal>
+                </td>
+              )
+            }
+            customColumnRenderer={{
+              status:(data)=>(
+                <div className={"flex items-center justify-center"}>
+                  <CategoryActiveSwitch
+                    categoryId={data.id as string}
+                    initialActive={data.status as boolean}
+                  />
+                </div>
+              )
+            }}
+          />
+        </div>
       </BasicCard.content>
     </BasicCard>
   );
