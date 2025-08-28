865b8433ada81dafbad2622ffdc12fe970dd192e
Azukii09 <azuknisianta@gmail.com>
2025-08-28 13:10:42 +0700

    #2, #7 - **feat(frontend): replace "Add New Category" button with modal in MainViewCategory**
    - Replaced the primary button with a modal for category creation in `MainViewCategory`.
    - Enhanced modal with header, body, and footer for improved functionality.

--- frontend/component/ui/categories/MainViewCategory.tsx
+++ frontend/component/ui/categories/MainViewCategory.tsx
@@ -29,7 +29,21 @@ export default function MainViewCategory() {
         haveFooter={false}
       >
         <div>
-          <Button buttonType={"button"} variant={"primary"} buttonName={"new-category"} buttonText={"Add New Category"} size={"sm"}/>
+          <Modal id={"mdl-add"} btnVariant={"primary"} btnName={"add-category"} btnText={"New Category"} btnSize={"sm"}>
+            <Modal.Header>
+              Add New Category
+            </Modal.Header>
+            <Modal.Body>body</Modal.Body>
+            <Modal.Footer
+              btnVariant={"success"}
+              btnText={"Submit"}
+              btnName={"Submit"}
+              btnVariantType={"solid"}
+              btnSize={"sm"}
+            >
+            </Modal.Footer>
+          </Modal>
+
           <Table
             data={dataInLocal}
             excludesColumnsData={[ "id"]}
