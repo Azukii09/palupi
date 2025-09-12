a8e2ebb704eed04d516de8b7a965cf640964de37
Azukii09 <azuknisianta@gmail.com>
2025-09-12 20:17:03 +0700

    #2 - **refactor(api): update route path and database connection details**
    - Changed route from `/items` to `/` in `category_router`.
    - Updated `get_item_handler` to use new database connection URL and removed path parameter.

--- backend/src/app/route.rs
+++ backend/src/app/route.rs
@@ -7,11 +7,11 @@ use crate::core::connections_pool;
 use crate::{connect_pool_new, list_categories};
 
 pub fn category_router() -> Router {
-    Router::new().route("/items", get(get_item_handler))
+    Router::new().route("/", get(get_item_handler))
 }
 
-async fn get_item_handler(axum::extract::Path(id): axum::extract::Path<i32>) -> impl axum::response::IntoResponse {
-    let pool = connect_pool_new("postgres://postgres:postgres@localhost:5432/postgres").await.unwrap();
+async fn get_item_handler() -> impl axum::response::IntoResponse {
+    let pool = connect_pool_new("postgres://admin:adminpalupi123456@localhost:5432/palupi_db").await.unwrap();
     match list_categories(&pool, "id").await {
         Ok(item) => axum::response::Json(json!(item)),
         Err(_) => axum::response::Json(json!({"error": "Item not found"})),
