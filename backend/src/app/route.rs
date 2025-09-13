5c07fb9e988be12d99d44603effe25bc1da8b55c
Azukii09 <azuknisianta@gmail.com>
2025-09-13 10:08:56 +0700

    #2 - **refactor(api): simplify category_router and remove unused handler**
    - Replaced `get_item_handler` with inline closure for `category_router`.
    - Removed unused `get_item_handler` logic and related database connection code.

--- backend/src/app/route.rs
+++ backend/src/app/route.rs
@@ -2,18 +2,8 @@ use axum::{
     routing::{ get},
      Router,
 };
-use serde_json::json;
-use crate::core::connections_pool;
-use crate::{connect_pool_new, list_categories};
 
 pub fn category_router() -> Router {
-    Router::new().route("/", get(get_item_handler))
+    Router::new().route("/", get(|| async { println!("Hello World"); }))
 }
 
-async fn get_item_handler() -> impl axum::response::IntoResponse {
-    let pool = connect_pool_new("postgres://admin:adminpalupi123456@localhost:5432/palupi_db").await.unwrap();
-    match list_categories(&pool, "id").await {
-        Ok(item) => axum::response::Json(json!(item)),
-        Err(_) => axum::response::Json(json!({"error": "Item not found"})),
-    }
-}
