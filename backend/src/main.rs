e0faf2fbb006874a54770dbc2cb0a79e7ef373bb
Azukii09 <azuknisianta@gmail.com>
2025-09-14 18:08:39 +0700

    #2 - **feat(api): add update_categories handler and route**
    - Implemented `update_categories` handler to update category details by ID.
    - Added `/update_categories/{id}` PUT route to the app router.
    - Updated `category.http` with a new test for the `/update_categories/{id}` endpoint.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use axum::{serve, Json, Router};
 use axum::extract::{Path, State};
 use axum::response::IntoResponse;
-use axum::routing::{delete, get, post};
+use axum::routing::{delete, get, post, put};
 use dotenvy::dotenv;
 use http::StatusCode;
 use serde::{Deserialize, Serialize};
@@ -77,6 +77,19 @@ async fn delete_categories(State(pool): State<PgPool>, Path(id): Path<i32>) -> i
     }
 }
 
+async fn update_categories(State(pool): State<PgPool>, Path(id): Path<i32>, Json(todo_req): Json<CategoryDto>) -> impl IntoResponse {
+    let rows: Vec<Category> = sqlx::query_as("SELECT * FROM categories WHERE id = $1").bind(&id).fetch_all(&pool).await.unwrap();
+
+    if rows.len() == 0 {
+        let msg = format!("No Categories id : {id} Found!");
+        (StatusCode::NOT_FOUND, msg)
+    } else {
+        sqlx::query("UPDATE categories SET name = $1 WHERE id = $2").bind(&todo_req.name).bind(&id).execute(&pool).await.unwrap();
+
+        (StatusCode::OK, "Update Categories Successful!".to_string())
+    }
+}
+
 async fn app () -> Router {
     let pool = db().await;
     Router::new()
@@ -84,6 +97,7 @@ async fn app () -> Router {
         .route("/categories/{:id}", get(get_single_categories))
         .route("/add_categories", post(add_categories))
         .route("/delete_categories/{:id}", delete(delete_categories))
+        .route("/update_categories/{:id}", put(update_categories))
         .with_state(pool)
 
 }
