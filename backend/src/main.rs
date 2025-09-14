1b73c653676fa11c8851f8b8d075f1f698b3902d
Azukii09 <azuknisianta@gmail.com>
2025-09-14 17:13:10 +0700

    #2 - **feat(main): add Category and CategoryDto structs to main.rs**
    - Introduced `Category` with `Serialize` and `FromRow` for DB mapping.
    - Added `CategoryDto` with `Deserialize` for input handling.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,6 @@
 use axum::{serve};
+use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
 use tokio::net::TcpListener;
 use crate::app::build_router;
 
@@ -12,4 +14,14 @@ async fn main() {
 
     let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
     serve(listener,app).await.unwrap()
+}
+
+#[derive(Serialize, FromRow)]
+struct Category {
+    id: i32,
+    name: String,
+}
+#[derive(Deserialize)]
+struct CategoryDto {
+    name: String,
 }
