5c0c3caf419de3b9527d20554c6f641eb5acd484
Azukii09 <azuknisianta@gmail.com>
2025-09-28 19:14:00 +0700

    #2 - **refactor(categories): remove redundant comment in `categoryCreateInitial`**
    - Cleaned up an unnecessary comment in `categoryCreateInitial.ts` to improve code readability.

--- frontend/features/categories/state/categoryCreateInitial.ts
+++ frontend/features/categories/state/categoryCreateInitial.ts
@@ -1,10 +1,8 @@
-// ✅ initial state: ok di-narrow literal
 import {ActionResultFrom} from "@/lib/type/actionType";
 import {CreateCategorySchema} from "@/features/categories/validations/validation";
 import {CategoryCreateSuccess} from "@/features/categories/services/category.type";
 
 export type CategoryCreateState = ActionResultFrom<typeof CreateCategorySchema, CategoryCreateSuccess>;
-
 
 export const categoryCreateInitial: CategoryCreateState = {
   ok: false as const,
