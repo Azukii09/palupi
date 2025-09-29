5922ae56dc060742f9e917c6e4eb82bd96392c98
Azukii09 <azuknisianta@gmail.com>
2025-09-29 17:07:59 +0700

    #2, #10 - **refactor(categories): add `UpdateCategorySchema` to validation for update handling**

--- frontend/features/categories/validations/validation.ts
+++ frontend/features/categories/validations/validation.ts
@@ -13,3 +13,17 @@ export const CreateCategorySchema = z.object({
   status: z.boolean()
     .optional(),
 });
+
+export const UpdateCategorySchema = z.object({
+  name: z.string()
+    .trim()
+    .min(4, "validation.name.min")
+    .max(120, "validation.name.max") ,
+  description: z.string()
+    .trim()
+    .min(4, "validation.description.min")
+    .max(120, "validation.description.max")
+    .optional(),
+  status: z.boolean()
+    .optional(),
+});
