ab70d12fdeda525a641d566ce52fa675d6e4dd1e
Azukii09 <azuknisianta@gmail.com>
2025-10-02 17:49:03 +0700

    #13 - **refactor(menu-items): enhance `MainMenuItems` layout with `BasicCard` component**
    - Replaced plain `div` structure with `BasicCard` for a more structured and styled layout.

--- frontend/features/menu_items/component/MainMenuItems.tsx
+++ frontend/features/menu_items/component/MainMenuItems.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import BasicCard from "@/component/util/base/BasicCard";
 
 export default function MainMenuItems() {
   return (
-    <div>
-      menu items
-    </div>
+    <BasicCard>
+      <BasicCard.title>
+        <h1 className="text-xl font-bold text-primary capitalize">
+          Menu Items
+        </h1>
+      </BasicCard.title>
+      <BasicCard.content
+        haveFooter={false}
+      >
+        MainMenuItems
+      </BasicCard.content>
+    </BasicCard>
   );
 }
